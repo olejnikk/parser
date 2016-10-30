0ed592d6a4fbc47532f68cd02368dab16bee395e
kolejnik <kolejnik.pl@gmail.com>
2016-10-30 14:39:39 +0100

    fix

--- src/main/java/com/kolejnik/parser/Main.java
+++ src/main/java/com/kolejnik/parser/Main.java
@@ -7,15 +7,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             System.out.print(">>> ");
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             String input = null;
             try {
                 input = br.readLine();
+                if ("quit".equals(input)) {
+                    br.close();
+                    System.exit(0);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -25,6 +30,8 @@ public class Main {
             Node node = parser.parse(tokens);
             System.out.println(">>> " + node.compute());
         }
+
+
 //        Lexer lexer = new Lexer();
 //        List<Token> tokens = lexer.tokenize("((3-12)*123 + 3^1)+(132-12)*(44-12)");
 //        for (Token token : tokens) {
